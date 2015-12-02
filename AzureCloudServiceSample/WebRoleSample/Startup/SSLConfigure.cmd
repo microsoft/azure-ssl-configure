ed14fc8b42201d1cbad035cc8c55af123be2658a
qinxgit <qinx@Microsoft.com>
2015-12-02 14:22:48 -0800

    add log

--- AzureCloudServiceSample/WebRoleSample/Startup/SSLConfigure.cmd
+++ AzureCloudServiceSample/WebRoleSample/Startup/SSLConfigure.cmd
@@ -1,3 +1,3 @@
-PowerShell -ExecutionPolicy Unrestricted .\Startup\SSLConfigure.ps1 -sco
+PowerShell -ExecutionPolicy Unrestricted .\Startup\SSLConfigure.ps1 -sco  >> "%TEMP%\StartupLog.txt" 2>&1
 
 EXIT /B 0
