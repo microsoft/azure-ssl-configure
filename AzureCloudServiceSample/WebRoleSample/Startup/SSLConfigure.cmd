fe1e323babb3dde29ff47d17c31d6624d85398dd
Ido Schreier <ido.schreier@gmail.com>
2016-12-15 10:39:25 +0200

    Skip script invocation in emulated environment

--- AzureCloudServiceSample/WebRoleSample/Startup/SSLConfigure.cmd
+++ AzureCloudServiceSample/WebRoleSample/Startup/SSLConfigure.cmd
@@ -1,3 +1,10 @@
-PowerShell -ExecutionPolicy Unrestricted .\Startup\SSLConfigure.ps1 -sco  >> "%TEMP%\StartupLog.txt" 2>&1
+SET LOG_FILE="%TEMP%\StartupLog.txt"
 
-EXIT /B 0
+if "%ComputeEmulatorRunning%" == "false" (
+	echo "Invoking SSLConfigure.ps1 on Azure service" >> %LOG_FILE% 2>&1	
+	PowerShell -ExecutionPolicy Unrestricted .\Startup\SSLConfigure.ps1 -sco  >> "%TEMP%\StartupLog.txt" 2>&1
+) else (
+	echo "Skipping SSLConfigure.ps1 invocation on emulated environment" >> %LOG_FILE% 2>&1	
+)    
+
+EXIT /B 0 
